e11de069b67991cca6764384ae9898c29ff14d13
Diego <waziri.diego@gmail.com>
2017-02-01 12:21:19 +0100

    add dossier src

---
+++ tests/Lib/stringFormatterTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace tests\Lib;
+
+use Lib\stringFormatterTest;
+
+class StringFormatterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $prefix
+     * @param string $string
+     * @param bool $camelCase
+     * @return string
+     */
+    public function prefix($prefix, $string, $camelCase = false)
+    {
+        if($camelCase)
+        {
+            return $this->toCamelCase($prefix, $string);
+        }
+        return $this->concatString($prefix, $string);
+    }
+
+    /**
+     * @param string $suffix
+     * @param string $string
+     * @param bool $camelCase
+     * @return string
+     */
+    public function suffix($suffix, $string, $camelCase = false)
+    {
+        if($camelCase)
+        {
+            return $this->toCamelCase($string, $suffix);
+        }
+        return $this->concatString($string, $suffix);
+    }
+
+    /**
+     * @param string $firstString
+     * @param string $secondString
+     * @return string
+     */
+    public function toCamelCase($firstString, $secondString)
+    {
+        return $this->concatString(strtolower($firstString), ucfirst($secondString));
+    }
+
+    /**
+     * @param string $firstString
+     * @param string $secondString
+     * @return string
+     */
+    public function concatString($firstString, $secondString)
+    {
+        return $firstString . $secondString;
+    }
+}
